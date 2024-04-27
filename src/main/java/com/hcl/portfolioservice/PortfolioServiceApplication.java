3be6d9a82a016983f401248e59b249ede5f4dce9
Mahendra <mahendrasridayarathna@gmail.com>
2024-04-27 10:20:25 +0530

    initial Project Setup Done

---
+++ src/main/java/com/hcl/portfolioservice/PortfolioServiceApplication.java
@@ -0,0 +1,18 @@
+package com.hcl.portfolioservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mahendrasridayarathna
+ * @created 27/04/2024 - 11:30 AM
+ * @project IntelliJ IDEA
+ */
+@SpringBootApplication
+public class PortfolioServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioServiceApplication.class, args);
+    }
+
+}
