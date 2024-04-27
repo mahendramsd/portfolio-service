18490d4e4a681f23345eecb96b573540abc785bd
akilamw <71129746+akilamw@users.noreply.github.com>
2024-04-27 11:51:51 +0530

    repository update

--- src/main/java/com/hcl/portfolioservice/domain/repository/PortfolioRepository.java
+++ src/main/java/com/hcl/portfolioservice/domain/repository/PortfolioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PortfolioRepository extends JpaRepository<Long, Portfolio> {
+public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
 }
